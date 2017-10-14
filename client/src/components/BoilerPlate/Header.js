d960ec2c89c14a5c879de61b79e0977bb5523bba
smpr <gcbrown1116@gmail.com>
2017-10-13 21:17:20 -0400

    started on header setup and primitive styling

--- client/src/components/BoilerPlate/Header.js
+++ client/src/components/BoilerPlate/Header.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 
+const BodyWrapper = styled.div`
+display: flex;
+flex-direction: column;
+
+
+`
+const UnderBodyWrapper = styled.div`
+display: flex;
+flex-direction: row;
+
+
+`
+const HeaderBlock = styled.div`
+border: 0;
+border-radius: 0px;
+width: 275px;
+height: 100px;
+margin: 10px auto;
+background-color: darkblue;
+text-align:center;
+color: white;
+flex: 1;
+
+}
+`
+const UnderHeaderBlock = styled.div`
+border: 0;
+border-radius: 0px;
+height: 50px;
+margin: 10px auto;
+background-color: darkblue;
+text-align:center;
+color: white;
+flex: 1;
+
+}
+`
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <BodyWrapper>
+                
+                    <HeaderBlock>
+                        App Name
+                    </HeaderBlock>
+                
+                <UnderBodyWrapper>
+                    <UnderHeaderBlock>
+                        Another
+                    </UnderHeaderBlock>
+              
+                    <UnderHeaderBlock>
+                        Another
+                    </UnderHeaderBlock>
+                </UnderBodyWrapper>
+            </BodyWrapper>
         );
     }
 }
