178c51f1506c5835b50cf407e050199dd6f2e79f
smpr <gcbrown1116@gmail.com>
2017-10-13 20:51:46 -0400

    Stylings nearly done on nav bar

--- client/src/components/BoilerPlate/Nav.js
+++ client/src/components/BoilerPlate/Nav.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+const BodyWrapper = styled.div`
+display: flex;
+justify-content: flex-start;
+flex-direction: row-wrap;
 
+
+`
+const NavBlock = styled.div`
+border: 0;
+border-radius: 0px;
+width: 275px;
+height: 20px;
+margin: 10px auto;
+background-color: darkblue;
+text-align:center;
+color: white;
+flex: 1;
+Link:{
+    text-decoration: none;
+}
+`
 class Nav extends Component {
     render() {
         return (
-            <div>
-                Nav
-            </div>
+            <BodyWrapper>
+                <NavBlock >
+                <Link to='#'>Back</Link>
+                </NavBlock>
+                <NavBlock>
+                <Link to='/'>Home</Link>
+                </NavBlock>
+                <NavBlock>
+                <Link to='/'>Articles</Link>
+                </NavBlock>
+                <NavBlock>
+                <Link to='/'>Whats New</Link>
+                </NavBlock>
+            </BodyWrapper>
         );
     }
 }
