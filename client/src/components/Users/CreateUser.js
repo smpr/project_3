1ec639256d626418a00a8fceaec1a65aa505d6ef
smpr <gcbrown1116@gmail.com>
2017-10-14 14:04:05 -0400

    user page now showing up!

--- client/src/components/Users/CreateUser.js
+++ client/src/components/Users/CreateUser.js
@@ -1,60 +1,64 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import axios from 'axios'
 
 class CreateUser extends Component {
-    state = {
-        user: [],
-        redirectToHome: false,
-        userId: ''
-      }
+  state = {
+    user: [],
+    redirectToUser: false,
+    userId: ''
+  }
 
-    handleChange = (event) => {
-        const attribute = event.target.name
-        const updateUser = { ...this.state.user }
-        updateUser[attribute] = event.target.value
-        this.setState({ user: updateUser })
+
+  handleChange = (event) => {
+    const attribute = event.target.name
+    const updateUser = {...this.state.user}
+    updateUser[attribute] = event.target.value
+    this.setState({user: updateUser})
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault()
+
+    const res = await axios.post('/api/users', {
+      'user': this.state.user
+    })
+    this.setState({redirectToUser: true, userId: res.data._id})
+  }
+
+  render () {
+    if (this.state.redirectToUser) {
+      return <Redirect to={`/user/${this.state.userId}`} />
     }
 
-    handleSubmit = async (event) => {
-        event.preventDefault()
-        const res = await axios.post('/api/users', {
-            'user': this.state.user
-        })
-        console.log(res.data)
-        this.setState({redirectToIdea: true, newUserId: res.data._id})
-    }
-
-
-    render() {
-        return (
-            <div>
-                <h1>Sign-Up</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <label htmlFor="firstName">First Name</label>
-                        <input
-                            onChange={this.handleChange} name="firstName"
-                            type="text" value={this.state.user.firstName}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="lastName">Last Name</label>
-                        <input
-                            onChange={this.handleChange} name="lastName"
-                            type="text" value={this.state.user.lastName}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <input onChange={this.handleChange}
-                            value={this.state.user.email}
-                            name="email" type="text" />
-                    </div>
-                    <button>Sign Up</button>
-                </form>
-            </div>
-        )
-    }
+    return (
+      <div>
+        <h1>Sign-Up</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="firstName">First Name</label>
+            <input
+              onChange={this.handleChange} name="firstName"
+              type="text" value={this.state.user.firstName}
+            />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last Name</label>
+            <input onChange={this.handleChange}
+              value={this.state.user.lastName}
+              name="lastName" type="text" />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input onChange={this.handleChange}
+              value={this.state.user.email}
+              name="Email" type="text" />
+          </div>
+          <button>Sign Up</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default CreateUser
